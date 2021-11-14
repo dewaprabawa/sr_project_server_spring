ae4e0803f30fbee6ae4d890dc0647705286f7442
osls <balipastika@gmail.com>
2021-11-14 17:14:53 +0800

    add jwt helper

---
+++ src/main/java/learn_rest/example/tell_me_this_will_work/auth/security/JwtUtils.java
@@ -0,0 +1,5 @@
+package learn_rest.example.tell_me_this_will_work.auth.security;
+
+public class JwtUtils {
+    
+}
--- src/main/java/learn_rest/example/tell_me_this_will_work/auth/service/UserDetailServiceImpl.java
+++ src/main/java/learn_rest/example/tell_me_this_will_work/auth/service/UserDetailServiceImpl.java
@@ -14,6 +14,6 @@ public class UserDetailServiceImpl implements UserDetailService{
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user =  userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
-        return  UserDetailsImpl.build(user);
+        return UserDetailsImpl.build(user);
     }
 }
